b970a5c78a3b63f1dd5e284cfd8512a8107f7daa
siqi <edu.foongs@gmail.com>
2020-06-15 17:12:26 +0800

    Microsoft authentication

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,4 @@
-import app from 'firebase/app';
+import firebase from 'firebase/app';
 import 'firebase/auth';
 import { Redirect } from 'react-router-dom';
 
@@ -11,21 +11,54 @@ export const config = {
     messagingSenderId: "477222218367",
     appId: "1:477222218367:web:86b0debd6dc0c5ba57ad4a",
     measurementId: "G-FHPWWN1DXM",
-    redirectUrl: 'https://tableaux-9cae1.firebaseapp.com/__/auth/handler'
   };
 
 class Firebase {
     constructor() {
-      app.initializeApp(config);
-      this.auth = app.auth();
+      firebase.initializeApp(config);
+      this.auth = firebase.auth();
     }
-    //Auth API
-    doSignInWithEmailAndPassword = (email, password) =>
-    this.auth.signInWithEmailAndPassword(email, password);
 
-    doSignOut = () => this.auth.signOut();
+    doSignIn = () => {
+    
+      var provider = new firebase.auth.OAuthProvider('microsoft.com');
 
+      provider.setCustomParameters({
+        // Force re-consent.
+        prompt: 'consent',
+        // Target specific email with login hint.
+        login_hint: 'user@u.nus.edu'
+      });
+
+      firebase.auth().signInWithRedirect(provider);
+
+      return firebase.auth().getRedirectResult()
+      .then(function(result) {
+        // User is signed in.
+        // IdP data available in result.additionalUserInfo.profile.
+        // OAuth access token can also be retrieved:
+        // result.credential.accessToken
+        // OAuth ID token can also be retrieved:
+        // result.credential.idToken
+        console.log(result.credential.idToken)
+      })
+      .catch(function(error) {
+        // Handle error.
+        console.log('Sign in error')
+      });
+    }
+    
+    
+  doSignOut = () => {
+    return firebase.auth().signOut().then(function() {
+      // Sign-out successful.
+    }).catch(function(error) {
+      // An error happened.
+    });
+  }
 }
+
+
 
 export default Firebase;
 
